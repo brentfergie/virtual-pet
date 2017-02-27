d661875984f2418e6d6e8dbb739905afb05f0915
brentfergie <brentfergie@sbcglobal.net>
2017-02-27 13:54:33 -0500

    first commit

--- VPPractice/Program.cs
+++ VPPractice/Program.cs
@@ -10,6 +10,63 @@ namespace VPPractice
     {
         static void Main(string[] args)
         {
+            string userResult = "0";
+
+            VPDog myDog = new VPDog();  
+
+            Console.WriteLine("Enter dog name"); // User name's dog and breed.  Only appears at beginning
+            myDog.name = Console.ReadLine();
+
+            Console.WriteLine("Enter dog breed");
+            myDog.breed = Console.ReadLine();
+
+            Console.WriteLine("Your dog's name is {0} and its breed is a {1}.", myDog.name, myDog.breed);
+            Console.WriteLine("Let's check {0}'s stats.", myDog.name);
+
+
+            Console.WriteLine("{0} likes his hunger, thirst, and boredom numbers to be below 5.", myDog.name);
+
+            while (userResult != "4") //Main console display for user to enter options
+            {
+                myDog.ShowStats();
+
+                ActionMenu();
+
+               Console.WriteLine("enter an option");
+
+                userResult = Console.ReadLine();
+                if (userResult == "1")
+                {
+                    myDog.FeedPet();
+
+                }
+                else if (userResult == "2")
+                {
+                    myDog.WaterPet();
+                }
+                else if (userResult == "3")
+                {
+                    myDog.PlayPet();
+                }
+                else
+                {
+                    break;
+                }
+
+            }
         }
+        public static void ActionMenu() //User choices for changing the dog's status levels
+        {
+            Console.WriteLine("\nChoose one of the following to make the dog happy:");
+            Console.WriteLine("1) Feed the dog");
+            Console.WriteLine("2) Fill the water bowl");
+            Console.WriteLine("3) Take the dog out to play");
+            Console.WriteLine("4) Quit");
+           
+        
+        }
+        
+
     }
+
 }
---
+++ VPPractice/VPDog.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPPractice
+{
+    class VPDog
+    {
+        public string name { get; set; } //Properties to set variables for VPDog class
+        public string breed { get; set; }
+
+        public int DogLevelHunger { get; set; }  //Properties to set levels for dog status
+        public int DogLevelThirst { get; set; }
+        public int DogLevelBoredom { get; set; }
+        
+        public VPDog() // Constructor for dog status levels
+        {
+            DogLevelHunger = 5;
+            DogLevelThirst = 5;
+            DogLevelBoredom = 5;
+       
+        }
+
+        public void FeedPet() //Methods for changing dog status levels when user enters a number
+        {
+            DogLevelHunger--;
+            DogLevelThirst++;
+
+            if (DogLevelHunger <= 2)
+            {
+                Bathroom();
+            }
+        }
+
+        public void WaterPet() 
+        {
+            DogLevelThirst--;
+            if (DogLevelThirst > 9)
+            {
+                Console.WriteLine("Fill the dog's water bowl!");
+            }
+        }
+        public void PlayPet() 
+        {
+            DogLevelBoredom--; 
+            DogLevelThirst++;
+        }
+        public void Bathroom()
+        {
+            DogLevelHunger++; 
+            Console.WriteLine("\nIf the dog's Hunger level goes below 2 it uses the bathroom \n and makes his hunger increase.");//explains hunger status change
+        }
+
+
+        public void ShowStats() //Method to show dog status levels
+        {
+            Console.WriteLine("\nHunger: " + DogLevelHunger);
+            Console.WriteLine("Thirst: " + DogLevelThirst);
+            Console.WriteLine("Boredom: " + DogLevelBoredom);
+        }
+    }
+    }
+     
+
+
